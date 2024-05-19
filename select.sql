3e92b06c16bb576a748ded735cfd0ed08838acf1
Yan da Costa Fernandes Gomes <143640083+yancfgomes@users.noreply.github.com>
2024-05-19 15:50:37 -0400

    Refactor select.sql queries for better readability and maintainability

--- select.sql
+++ select.sql
@@ -4,10 +4,26 @@
 
 use cadastro; -- seleciona o banco de dados 
 
-select * from cursos; -- mostra a table <cursos>
+-- MOSTRANDO TODAS AS COLUNAS DA TABELA <curso>
+select * from cursos; 
+
+-- MOSTRANDO TODAS AS COLUNAS DA TABELA <CURSO> PELA ORDEM DO CAMPO <NOME>
+select * from cursos
+order by nome; -- ou ~order by nome asc;
+
+-- MOSTRA A TABLE <CURSO> PELA ORDEM DO CAMPO <NOME> DE FORMA INVERSA
+select * from cursos
+order by nome desc; 
+
+-- MOSTRANDO APENAS AS COLUNAS CITADAS <NOME,CARGA,ANO> DA TABELA E ORDENANDO PELOS CAMPOS <ANO,NOME>
+select ano,nome,carga from cursos
+order by ano,nome;
+
+-- > SELECIONANDO LINHAS < --
 
 select * from cursos
-order by nome; -- mostra a table <cursos> pela ordem do campo <nome> 
+where ano ='2016' -- operadores relacionais: = / > / < / >= / <= / != ou <> / between <valor1> and <valor2> / in (<valor1>,<valor2>,<valor3>) / and / or
+order by nome;
 
-select * from cursos
-order by nome desc; -- mostra a table <cursos> pela ordem do campo <nome> de forma INVERSA
+
+
